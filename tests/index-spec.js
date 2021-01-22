ffa834ed4be76dee5abf3e36ef7a1f27955aedaa
Rachael McIlhagga <rachael.mcilhagga@live.co.uk>
2021-01-21 21:22:26 -0500

    second push

--- tests/index-spec.js
+++ tests/index-spec.js
@@ -7,7 +7,7 @@ console.log('running test: against server.js');
 
 describe('title: This is the title of the test', () => {//describe the thing it is testing (title)
     it('this simple test checks equality for a string', () => { // function that executes the test
-        expect('Test with Travis').to.equal('Test CI with Travis');//this is the epectation. expected outcome
+        expect('Test CI with Travis').to.equal('Test CI with Travis');//this is the epectation. expected outcome
     });
     // process.exit();
 });
